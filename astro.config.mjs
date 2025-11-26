18b0c87317541577283ae255576adc60b9734052
SpaceGhost <lukasz.celitan@gmail.com>
2025-11-26 19:55:54 +0100

    fix: site nane

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-    site: 'https://massivdash.github.io',
-    base: '/bozenacelitan',
+    site: 'https://massivdash.github.io/',
+    base: '//BozenaCelitan',
 })
